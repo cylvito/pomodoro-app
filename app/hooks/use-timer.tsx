30c61a0cbe3ab0c0b2e54eeb1f808ec0e7a75977
Cylvia <cylvia.lian@gmail.com>
2025-07-12 10:16:03 -0700

    fuctional pomodoro

--- app/hooks/use-timer.tsx
+++ app/hooks/use-timer.tsx
@@ -1,34 +1,74 @@
 'use client';
-import { useActionState, useEffect, useState } from "react";
+import { useActionState, useCallback, useEffect, useState } from "react";
 type TimerMode = "focus" | "break";
 // Time to minutes
-const focusTime = 25;
+const focusTime = 1;
 const breakTime = 5;
 
 export default function useTimer() {
     const [mode, setMode] = useState<TimerMode>('focus');
     const [timeLeft, setTimeLeft] = useState(focusTime * 60);
-    
+    const [isActive, setIsActive] =useState<boolean>(false);
+    const [sessions, setSessions] =useState(0);
 
+    const getDuration= useCallback((timerMode: TimerMode) => {
+        return timerMode === "focus" ? focusTime * 60 : breakTime * 60;
+    },[])
+
+    const resetTimer = () => {
+        setIsActive(false);
+        setTimeLeft(getDuration(mode))
+        }
 
     const toggleTimer = () => {
+        setIsActive(prev => !prev)
 
     }
 
+    const switchMode = useCallback(() => {
+        const newMode = mode === "focus" ? "break" : "focus";
+        setMode(newMode);
+        setTimeLeft(getDuration(newMode));
+        setIsActive(false);
+
+        // increment sesh
+        if(mode === "focus") {
+            setSessions((prev) => prev + 1)
+        }
+    }, [mode, getDuration])
+
     useEffect(() => {
-        const interval = setInterval(() => {
+
+        let interval: NodeJS.Timeout | null = null;
+
+        if(isActive) {
+            interval = setInterval(() => {
             setTimeLeft((prev) => {
                 return prev - 1;
             })
         }, 1000);
+        } else if(interval) {
+            clearInterval(interval);
+        }
 
         return () => {
             if(interval) clearInterval(interval);
         }
-    },[])
+    },[isActive])
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            switchMode();
+        }
+    }, [timeLeft, switchMode])
   
     return {
     mode,
     timeLeft,
+    toggleTimer,
+    isActive,
+    resetTimer,
+    switchMode,
+    sessions
   }
 }
--- components/timer.tsx
+++ components/timer.tsx
@@ -1,31 +1,59 @@
-'use client'
+"use client";
 import useTimer from "@/app/hooks/use-timer";
 
-
 export default function Timer() {
-  const { mode, timeLeft, toggleTimer } = useTimer();
+  const {
+    mode,
+    timeLeft,
+    toggleTimer,
+    isActive,
+    resetTimer,
+    switchMode,
+    sessions,
+  } = useTimer();
+  console.log(mode);
   // Time format MM:SS
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
-  const formattedTime = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
 
   return (
     <div className="max-w-sm bg-white shadow-xl w-full p-6 rounded-2xl">
-     <div className="flex items-center justify-between mb-4">
-      <h2 className="text-xl font-medium capitalize">Focus time</h2>
-      <div className="text-sm text-gray-500"> Sessions: 1 </div>
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium capitalize">
+          {mode === "focus" ? "Focus Time" : "Breakie Time"}
+        </h2>
+        <div className="text-sm text-gray-500"> Sessions: {sessions} </div>
       </div>
-     <div className="text-center text-6xl font-bold mb-6 text-red-500">
-     {formattedTime}
+      <div
+        className={`text-center text-6xl font-bold mb-6 ${
+          mode === "focus" ? "text-red-500" : "text-green-500"
+        }`}
+      >
+        {formattedTime}
+      </div>
+      <div className="flex justify-center space-x-4 mb-4">
+        <button
+          onClick={toggleTimer}
+          className={`${ isActive ? "bg-yellow-500 hover:bg-yellow-600" : "bg-blue-500 hover:bg-blue-600"} px-5 py-2 rounded-lg text-white`}
+        >
+          {isActive ? "Pause" : "Start"}
+        </button>
+        <button
+          onClick={resetTimer}
+          className="bg-grey-200 hover:bg-grey-300 px-5 py-2 rounded-lg text-grey"
+        >
+          Reset
+        </button>
+      </div>
+      <button
+        onClick={switchMode}
+        className="w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-grey py-2"
+      >
+        {mode === "focus" ? "Switch to Break" : "Switch to Focus"}{" "}
+      </button>
     </div>
-    <div className="flex justify-center space-x-4 mb-4">
-      <button onClick={toggleTimer} className="bg-yellow-500 hover:bg-yellow-600 px-5 py-2 rounded-lg text-white">Start</button>
-      <button className="bg-grey-200 hover:bg-grey-300 px-5 py-2 rounded-lg text-grey">Reset</button>
-    </div>
-    <button className="w-full bg-gray-100 hover:bg-gray-200 rounded-lg text-grey py-2">Switch to Break</button>
-    </div>
-
-    
-    
-  )
+  );
 }
